b27e2eeee11b51d305713ccdebaffa764516859f
noelleyabami-lgtm <noelleyabami@gmail.com>
2025-12-03 01:07:02 -0500

    Update server.py

--- MiBot/server.py
+++ MiBot/server.py
@@ -72,6 +72,6 @@ async def chat(msg: Message):
 
     return {"reply": bot_reply}
 
-
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="127.0.0.1", port=8000)
+    uvicorn.run("server:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
