669f20759354992c5bc940a53d2be94fcbe2f7e1
noelleyabami-lgtm <noelleyabami@gmail.com>
2025-12-03 20:07:04 -0500

    Update server.py

--- MiBot/server.py
+++ MiBot/server.py
@@ -5,8 +5,7 @@ import uvicorn
 from openai import OpenAI
 import json
 import os
-
-client = OpenAI(api_key="OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 app = FastAPI()
 
